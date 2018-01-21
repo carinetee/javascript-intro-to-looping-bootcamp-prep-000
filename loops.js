7ee08bba8bed7082e32843808fc72b98ac62ffba
Carine Tomassian <carinetomassian@gmail.com>
2018-01-21 07:22:47 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -10,9 +10,9 @@ function forLoop(array) {
 }
 
 
-function whileLoop(number) {
-  while (number > 0) {
-    --number;
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--number);
   }
   return "done";
 }
